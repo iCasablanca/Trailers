2afb58246428c50e5b2985dddfca7c170aa36291
Lucas Harding <lucas@lucasharding.com>
2010-11-20 02:11:17 -0500

    Fixed missing files

---
+++ Classes/iPhone/TrailersAppDelegate_iPhone.h
@@ -0,0 +1,18 @@
+//
+//  TrailersAppDelegate_iPhone.h
+//  Trailers
+//
+//  Created by Lucas Harding on 10-11-18.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TrailersAppDelegate.h"
+#import "TrailersViewController.h"
+
+@interface TrailersAppDelegate_iPhone : TrailersAppDelegate {
+    TrailersViewController *view;
+}
+
+
+@end
